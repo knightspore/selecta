3bb730609afaa67c464036c61d4909dcbb5921cb
Ciaran <cslemon96@gmail.com>
2023-05-03 02:32:52 +0200

    fix(LoginButton): remove dependencies

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,14 +1,7 @@
 import { useState } from "react";
-import { spotifyApi } from "@/lib/spotify/utils";
 
 export default async function LoginButton() {
   const [loginUrl, setLoginUrl] = useState<string | null>(null);
-
-  if (!loginUrl) {
-    const url = spotifyApi.createAuthorizeURL(["user-read-email"], "login");
-    setLoginUrl(url);
-  }
-
   return (
     <a href={loginUrl ?? ""}>
       {loginUrl}
