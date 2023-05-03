c99911054150926951b1d04f1408c086aa412bfb
Ciaran <cslemon96@gmail.com>
2023-05-03 18:24:39 +0200

    chore(lint)

--- src/app/api/artists/route.ts
+++ src/app/api/artists/route.ts
@@ -1,8 +1,8 @@
-import {SpotifyClient} from "@/lib/spotify";
-import {NextRequest, NextResponse} from "next/server";
+import { SpotifyClient } from "@/lib/spotify";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const { data } = await req.json()
-  const artists = await SpotifyClient.Artists(data)
-  return NextResponse.json(artists)
+  const { data } = await req.json();
+  const artists = await SpotifyClient.Artists(data);
+  return NextResponse.json(artists);
 }
