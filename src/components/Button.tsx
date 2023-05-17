e547c670cb0a467a8d12c5b30b13af55c333f9d4
Ciaran <cslemon96@gmail.com>
2023-05-17 02:14:15 +0200

    refactor(styles/input): Touch up input styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,9 @@ export default function Button({
     <button
       type={type}
       onClick={onClick}
-      className={`p-1 py-px text-center border-2 rounded bg-shell-200 border-shell-300 ${disabled && "text-shell-900/50"}`}
+      className={`p-2 py-px text-center border-2 rounded bg-shell-200 disabled:border-shell-200 hover:bg-shell-300 transition-all duration-150 border-shell-300 ${
+        disabled && "text-shell-900/50"
+      }`}
       disabled={disabled}
     >
       {text}
