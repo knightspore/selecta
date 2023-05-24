b3ddb42907b31742bb30d52193eff21d59dbb149
Ciaran <cslemon96@gmail.com>
2023-05-24 16:38:51 +0200

    refactor(tracks/features/FeatureTag): Cleanup

--- src/components/tracks/Features/FeatureTag.tsx
+++ src/components/tracks/Features/FeatureTag.tsx
@@ -1,8 +1,8 @@
-export default function FeatureTag({
-  children,
-}: {
+type Props = {
   children: React.ReactNode;
-}) {
+};
+
+export default function FeatureTag({ children }: Props) {
   return (
     <div>
       <p className="px-1 py-px text-xs font-medium rounded-full select-none bg-shell-400 text-shell-100">
