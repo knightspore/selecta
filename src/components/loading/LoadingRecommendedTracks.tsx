1a71bc106b6316dfdf864ff65f8d6863b7cf659e
Ciaran <cslemon96@gmail.com>
2023-05-24 16:32:42 +0200

    refactor(recommendations/RecommendedTracks): Extract loading component

---
+++ src/components/loading/LoadingRecommendedTracks.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "./LoadingSpinner";
+
+export default function LoadingRecommendedTracks() {
+  return (
+    <div className="relative col-span-1 sm:col-span-2 lg:col-span-3 xl:col-span-4 2xl:col-span-5">
+      <LoadingSpinner />
+    </div>
+  );
+}
--- src/components/recommendations/RecommendedTracks.tsx
+++ src/components/recommendations/RecommendedTracks.tsx
@@ -3,17 +3,13 @@
 import { Track as TrackType } from "@/lib/spotify/types";
 import { useRecommendationsContext } from "@/provider/RecommendationsProvider";
 import Track from "@/components/tracks/Track";
-import LoadingSpinner from "@/components/loading/LoadingSpinner";
+import LoadingRecommendedTracks from "../loading/LoadingRecommendedTracks";
 
 export default function RecommendedTracks() {
   const { recommendations, isLoading } = useRecommendationsContext();
 
   if (isLoading) {
-    return (
-      <div className="relative col-span-1 sm:col-span-2 lg:col-span-3 xl:col-span-4 2xl:col-span-5">
-        <LoadingSpinner />
-      </div>
-    );
+    return <LoadingRecommendedTracks />
   }
 
   return (
