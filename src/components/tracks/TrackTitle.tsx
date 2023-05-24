1abab4ceb7af462f0c760ca9ebe3970ff1fa8f0a
Ciaran <cslemon96@gmail.com>
2023-05-24 16:48:29 +0200

    refactor(tracks/TrackTitle): Cleanup

--- src/components/tracks/TrackTitle.tsx
+++ src/components/tracks/TrackTitle.tsx
@@ -1,8 +1,8 @@
-export default function TrackTitle({
-  name,
-}: {
+type Props = {
   name: string;
-}) {
+};
+
+export default function TrackTitle({ name }: Props) {
   return (
     <h2 className="text-sm font-bold md:text-base line-clamp-1">{name}</h2>
   );
