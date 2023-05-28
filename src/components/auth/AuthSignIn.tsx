5b832add66ea26aa57dfd2c19ee553983a3536d6
Ciaran <cslemon96@gmail.com>
2023-05-28 22:57:13 +0200

    wip(AuthSignIn): Redo Sign In Page

--- src/components/auth/AuthSignIn.tsx
+++ src/components/auth/AuthSignIn.tsx
@@ -1,15 +1,32 @@
 import { signIn } from "next-auth/react";
 import Button from "../Button";
+import { Suspense } from "react";
+import ExampleTrack from "@/app/(pages)/about/ExampleTrack";
 
 export default function AuthSignIn() {
-
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-        <Button
-          text="Sign In"
-          type="button"
-          onClick={() => signIn("spotify")}
-        />
+    <div className="flex flex-col items-center justify-center w-screen h-screen text-shell-900 gap-2 bg-gradient-to-tr from-shell-300 via-coral-100">
+      <div className="p-4 mx-auto text-center prose">
+        <h1>
+          Welcome to <span>Selecta</span>
+        </h1>
+        <p>
+          Selecta is here to help you understand the music that{" "}
+          <i>you already know</i> better, and speak that same language right
+          back to Spotify to find whatever it is you can&apos;t put words to.
+        </p>
+        <Suspense>
+          {/* @ts-ignore */}
+          <ExampleTrack />
+        </Suspense>
+        <div className="mt-4 not-prose">
+          <Button
+            text="🧬 Sign In"
+            type="button"
+            onClick={() => signIn("spotify")}
+          />
+        </div>
+      </div>
     </div>
   );
 }
